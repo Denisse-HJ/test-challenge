db23056aaa320f5d610674f3c238d2496fcf68a0
Denisse-HJ <arq.denisseher@gmail.com>
2024-01-22 18:49:53 -0600

    aprobado dna

--- tests/dna.test.js
+++ tests/dna.test.js
@@ -3,7 +3,7 @@ const dna = require('../dna.js');
 describe('describe dna', () => {
   it('Recibe un dna que cumpla con CTAGGGTA', () => {
     expect(dna('CTAGGGTA')).toBe('CTAGGGTA');
-    expect(dna('cTAgGGtA')).toBe('CTAGGTA');
+    expect(dna('cTAgGGtA')).toBe('TAGGA');
     expect(dna('ACGTacgt')).toBe('ACGT');
     expect(dna('ACGTCGGacgt456C')).toBe('ACGTCGGC');
   });
